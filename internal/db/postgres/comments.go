8eac20bb8791cf04fcd572a6ebb3a9cebc61b046
kudrin-chris <c-cud@mail.ru>
2024-06-23 21:50:23 +0300

    add postgres post and comment

--- internal/db/postgres/comments.go
+++ internal/db/postgres/comments.go
@@ -1,6 +1,8 @@
 package postgres
 
 import (
+	"server/graph/model"
+
 	"github.com/jmoiron/sqlx"
 )
 
@@ -11,12 +13,47 @@ type CommentsPostgres struct {
 func NewCommentsPostgres(db *sqlx.DB) *CommentsPostgres {
 	return &CommentsPostgres{db: db}
 }
-func CreateComment() {
+
+func (c CommentsPostgres) CreateComment(comment model.Comment) (model.Comment, error) {
+
+	tx, err := c.db.Begin()
+	if err != nil {
+		return model.Comment{}, err
+	}
+
+	sqlText := "INSERT INTO comments (content, author, post, reply_to) VALUES ($1, $2, $3, $4) RETURNING id"
+	row := tx.QueryRow(sqlText, comment.Content, comment.Author, comment.Post, comment.ReplyTo)
+	if err := row.Scan(&comment.ID); err != nil {
+		tx.Rollback()
+		return model.Comment{}, err
+	}
+
+	return comment, tx.Commit()
 
 }
-func CommentsSubscription() {
 
+func (c CommentsPostgres) GetCommentsByPost(postId, limit int) ([]*model.Comment, error) {
+
+	sqlText := "SELECT * FROM comments WHERE post = $1 AND reply_to IS NULL ORDER BY created_at LIMIT $2"
+
+	var comments []*model.Comment
+
+	if err := c.db.Select(&comments, sqlText, limit); err != nil {
+		return nil, err
+	}
+
+	return comments, nil
 }
-func SubscriptionResolver() {
 
+func (c CommentsPostgres) GetRepliesOfComment(commentId int) ([]*model.Comment, error) {
+
+	sqlText := "SELECT * FROM comments WHERE reply_to = $1"
+
+	var comments []*model.Comment
+
+	if err := c.db.Select(&comments, sqlText, commentId); err != nil {
+		return nil, err
+	}
+
+	return comments, nil
 }
--- internal/db/postgres/posts.go
+++ internal/db/postgres/posts.go
@@ -1,6 +1,9 @@
 package postgres
 
 import (
+	"server/graph/model"
+	"strconv"
+
 	"github.com/jmoiron/sqlx"
 )
 
@@ -12,12 +15,37 @@ func NewPostsPostgres(db *sqlx.DB) *PostsPostgres {
 	return &PostsPostgres{db: db}
 }
 
-func CreatePost() {
+func (p PostsPostgres) CreatePost(post model.Post) (model.Post, error) {
+	sqlText := "INSERT INTO Posts (name, content, author, comments_allowed) VALUES ($1, $2, $3, $4) RETURNING id, created_at"
+	tx, err := p.db.Begin()
+	if err != nil {
+		return model.Post{}, err
+	}
+	row := tx.QueryRow(sqlText, post.Name, post.Content, post.Author, post.CommentsAllowed)
+	if err := row.Scan(&post.ID); err != nil {
+		tx.Rollback()
+		return model.Post{}, err
+	}
 
+	return post, tx.Commit()
 }
-func GetAllPosts() {
+func (p PostsPostgres) GetAllPosts(limit int) ([]model.Post, error) {
+	sqlText := "SELECT * FROM post ORDER BY id LIMIT " + strconv.Itoa(limit)
 
+	var posts []model.Post
+	if err := p.db.Get(&posts, sqlText); err != nil {
+		return posts, err
+	}
+	return posts, nil
 }
-func GetPostByID() {
+func (p PostsPostgres) GetPostByID(id int) (model.Post, error) {
 
+	sqlText := "SELECT * FROM post WHERE id = " + strconv.Itoa(id)
+
+	var post model.Post
+	if err := p.db.Get(&post, sqlText); err != nil {
+		return post, err
+	}
+
+	return model.Post{}, nil
 }
